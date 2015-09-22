b452847b5485284cb333d908db5e30b582bf5fc9
Licong <licongdavis@hotmail.com>
2015-09-21 21:33:24 -0400

    Update index

--- index.php
+++ index.php
@@ -7,19 +7,18 @@ $sex = isset($_GET['sex']) ? $_GET['sex'] : '';
 $gpa = isset($_GET['gpa']) ? $_GET['gpa'] : '';
 $ielts = isset($_GET['ielts']) ? $_GET['ielts'] : '';
 
-if ($sex == 1){
-	echo "huxiling ni bu deng zhe!";
-}
-
 define('scoreLargest', 10);
 
 $scoreLargest = 9;
 
-echo start();
-
+start($sex);
 
 function start($sex){
-	echo 'huxiling ni deng zhe!';
+	if ($sex == 1){
+	echo "YORK UNIVERSITY";
+	}else{
+	echo "BLACKBERRY UNIVERSITY";	
+	}
 }
 
 ?>
