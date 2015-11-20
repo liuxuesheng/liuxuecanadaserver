70f84fc8123947e7a8ad46c0f974f0c232eb0094
ghe <ghe@blackberry.com>
2015-11-20 15:00:59 -0500

    fix issues for charts

--- tests/test1/test1page99.php
+++ tests/test1/test1page99.php
@@ -3,25 +3,32 @@ header("Content-Type:text/html;charset=utf-8");
 
 function returnPage99(){
 $item0 = array('id' => 9900,
-    'type' => 'piechart',
-    'name' => '毕业院校',
+    'type' => 'textview',
+    'name' => '选校报告',
     'relation' => '0',
     'relationid' => '0',
     'size' => 24,
     'textcolor' => '#FFFFFF',
     'alignment' => '14,15',
-    'inlayout' => 'middle');
+    'inlayout' => 'top');
 
 $item1 = array('id' => 9901,
-    'type' => 'radarchart',
-    'name' => '毕业院校',
+    'type' => 'piechart',
+    'name' => 'piechart1',
     'relation' => '0',
     'relationid' => '0',
-    'size' => 24,
-    'textcolor' => '#FFFFFF',
-    'alignment' => '14,15',
+    'alignment' => '14',
     'inlayout' => 'middle');
-$list = array($item0,$item1);
+
+$item2 = array('id' => 9902,
+    'type' => 'radarchart',
+    'name' => 'radarchart1',
+    'relation' => '3',
+    'relationid' => '9901',
+    'alignment' => '14',
+    'inlayout' => 'middle');
+
+$list = array($item0,$item1,$item2);
 	echo json_encode($list);
 }
 
