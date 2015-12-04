0cc28f8d819b685f27b6ce17c1b0ad4d3616e1ea
Licong Tang <litang@blackberry.com>
2015-12-04 15:09:29 -0500

    Update news image for category

--- news/news_list.php
+++ news/news_list.php
@@ -15,17 +15,20 @@ if ($conn->connect_error) {
 } 
 
 
-$sql = "SELECT id,news_title FROM news ORDER BY id DESC LIMIT 6";
+$sql1 = "SELECT id,news_title,news_imageURL FROM news ORDER BY id DESC LIMIT 1";
+$sql2 = "SELECT id,news_title FROM news ORDER BY id DESC LIMIT 1, 4";
 mysqli_query($conn, "SET CHARACTER SET 'utf8';");
 
-$result = $conn->query($sql);
-$news_title = "";
+$result1 = $conn->query($sql1);
+$result2 = $conn->query($sql2);
 $list = array();
+$row1 = $result1->fetch_assoc();
+$record1 = array('id' => $row1["id"], 'news_title' => $row1["news_title"], 'news_imageURL' => $row1["news_imageURL"]);
+array_push($list, $record1);
 
-
-if ($result->num_rows > 0) {
+if ($result2->num_rows > 0) {
     // output data of each row
-    while($row = $result->fetch_assoc()) {
+    while($row = $result2->fetch_assoc()) {
         $record = array('id' => $row["id"], 'news_title' => $row["news_title"]);
         array_push($list, $record);
     }
