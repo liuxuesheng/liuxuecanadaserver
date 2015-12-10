d3fc169f110eddb311e6748d692a3b7974fdce63
ghe <ghe@blackberry.com>
2015-12-10 11:32:48 -0500

    add new methods for news_list.php and create 2 news

--- news/news_list.php
+++ news/news_list.php
@@ -2,116 +2,114 @@
 header("Content-Type:text/html;charset=utf-8");
 
 function returnNewsList(){
-
-$user = 'root';
-$password = '1ppVlcA9';
-$db = 'liuxuecanada';
-
-// Create connection
-$conn = new mysqli('localhost',$user, $password, $db);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
 $list = array();
 
 //主板
-$sql88 = "SELECT id,news_title,news_imageURL FROM news ORDER BY id DESC LIMIT 1, 5";
-mysqli_query($conn, "SET CHARACTER SET 'utf8';");
-
-$result88 = $conn->query($sql88);
-
-if ($result88->num_rows > 0) {
-    // output data of each row
-    while($row88 = $result88->fetch_assoc()) {
-        $record88 = array('item_section' => 'news', 'id' => $row88["id"], 'news_title' => $row88["news_title"], 'news_imageURL' => $row88["news_imageURL"]);
-        array_push($list, $record88);
-    }
-}
+ $sql88 = "SELECT id,news_title,news_imageURL FROM news ORDER BY id DESC LIMIT 1, 5";
+getNewsHasImage($sql88,$list,'news');
 
 //学校介绍
 array_push($list, array('section' => '学校介绍'));
-
 $sql1 = "SELECT id,news_title,news_imageURL FROM news_university ORDER BY id DESC LIMIT 1";
 $sql2 = "SELECT id,news_title FROM news_university ORDER BY id DESC LIMIT 1, 4";
-
-$result1 = $conn->query($sql1);
-$result2 = $conn->query($sql2);
-
-if ($result1->num_rows > 0) {
-    // output data of each row
-    while($row1 = $result1->fetch_assoc()) {
-        $record1 = array('item_section' => 'news_university', 'id' => $row1["id"], 'news_title' => $row1["news_title"], 'news_imageURL' => $row1["news_imageURL"]);
-        array_push($list, $record1);
-    }
-}
-
-if ($result2->num_rows > 0) {
-    // output data of each row
-    while($row = $result2->fetch_assoc()) {
-        $record = array('item_section' => 'news_university', 'id' => $row["id"], 'news_title' => $row["news_title"]);
-        array_push($list, $record);
-    }
-}
+getNewsHasImage($sql1,$list,'news_university');
+getNewsNoImage($sql2,$list,'news_university');
 
 //专业选择
 array_push($list, array('section' => '专业选择'));
-
 $sql3 = "SELECT id,news_title,news_imageURL FROM news_program ORDER BY id DESC LIMIT 1";
 $sql4 = "SELECT id,news_title FROM news_program ORDER BY id DESC LIMIT 1, 4";
-
-$result3 = $conn->query($sql3);
-$result4 = $conn->query($sql4);
-
-if ($result3->num_rows > 0) {
-    // output data of each row
-    while($row3 = $result3->fetch_assoc()) {
-        $record3 = array('item_section' => 'news_program', 'id' => $row3["id"], 'news_title' => $row3["news_title"], 'news_imageURL' => $row3["news_imageURL"]);
-        array_push($list, $record3);
-    }
-}
-
-if ($result4->num_rows > 0) {
-    // output data of each row
-    while($row4 = $result4->fetch_assoc()) {
-        $record4 = array('item_section' => 'news_program', 'id' => $row4["id"], 'news_title' => $row4["news_title"]);
-        array_push($list, $record4);
-    }
-}
+getNewsHasImage($sql3,$list,'news_program');
+getNewsNoImage($sql4,$list,'news_program');
 
 //签证信息
 array_push($list, array('section' => '签证信息'));
-
 $sql5 = "SELECT id,news_title,news_imageURL FROM news_visa ORDER BY id DESC LIMIT 1";
 $sql6 = "SELECT id,news_title FROM news_visa ORDER BY id DESC LIMIT 1, 4";
+getNewsHasImage($sql5,$list,'news_visa');
+getNewsNoImage($sql6,$list,'news_visa');
 
-$result5 = $conn->query($sql5);
-$result6 = $conn->query($sql6);
-
-if ($result5->num_rows > 0) {
-    // output data of each row
-    while($row5 = $result5->fetch_assoc()) {
-        $record5 = array('item_section' => 'news_visa', 'id' => $row5["id"], 'news_title' => $row5["news_title"], 'news_imageURL' => $row5["news_imageURL"]);
-        array_push($list, $record5);
-    }
-}
-
-if ($result6->num_rows > 0) {
-    // output data of each row
-    while($row6 = $result6->fetch_assoc()) {
-        $record6 = array('item_section' => 'news_visa', 'id' => $row6["id"], 'news_title' => $row6["news_title"]);
-        array_push($list, $record6);
-    }
-}
 
 //学业心得
+array_push($list, array('section' => '学业心得'));
+$sql5 = "SELECT id,news_title,news_imageURL FROM news_experience ORDER BY id DESC LIMIT 1";
+$sql6 = "SELECT id,news_title FROM news_experience ORDER BY id DESC LIMIT 1, 4";
+getNewsHasImage($sql5,$list,'news_experience');
+getNewsNoImage($sql6,$list,'news_experience');
 
 //移民入籍新闻
+array_push($list, array('section' => '移民入籍新闻'));
+$sql5 = "SELECT id,news_title,news_imageURL FROM news_immigration ORDER BY id DESC LIMIT 1";
+$sql6 = "SELECT id,news_title FROM news_immigration ORDER BY id DESC LIMIT 1, 4";
+getNewsHasImage($sql5,$list,'news_immigration');
+getNewsNoImage($sql6,$list,'news_immigration');
 
 //中介服务信息
+array_push($list, array('section' => '中介服务信息'));
+$sql5 = "SELECT id,news_title,news_imageURL FROM news_agent ORDER BY id DESC LIMIT 1";
+$sql6 = "SELECT id,news_title FROM news_agent ORDER BY id DESC LIMIT 1, 4";
+getNewsHasImage($sql5,$list,'news_agent');
+getNewsNoImage($sql6,$list,'news_agent');
 
 	echo json_encode($list);
 }
-echo(returnNewsList())
+
+
+//getNewsHasImage 负责从数据库读取带有缩略图的新闻
+function getNewsHasImage($mysql, &$list, $section){
+    $user = 'root';
+    $password = '1ppVlcA9';
+    $db = 'liuxuecanada';
+    
+    // Create connection
+    $conn = new mysqli('localhost',$user, $password, $db);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+
+    $sql = $mysql;
+    mysqli_query($conn, "SET CHARACTER SET 'utf8';");
+
+    $result = $conn->query($sql);
+    
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            $nametring = array('item_section' => $section, 'id' => $row["id"], 'news_title' => $row["news_title"], 'news_imageURL' => $row["news_imageURL"]);
+            array_push($list, $nametring);
+        }
+    }
+}
+
+//getNewsNoImage 负责从数据库读取带有没有缩略图的新闻
+function getNewsNoImage($mysql, &$list, $section){
+    $user = 'root';
+    $password = '1ppVlcA9';
+    $db = 'liuxuecanada';
+
+    // Create connection
+    $conn = new mysqli('localhost',$user, $password, $db);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+
+    $sql = $mysql;
+    mysqli_query($conn, "SET CHARACTER SET 'utf8';");
+
+    $result = $conn->query($sql);
+    
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            $record = array('item_section' => $section, 'id' => $row["id"], 'news_title' => $row["news_title"]);
+            array_push($list, $record);
+        }
+    }
+
+}
+ echo(returnNewsList());
 ?>
